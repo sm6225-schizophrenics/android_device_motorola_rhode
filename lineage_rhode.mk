57604bf1506519b102c8885d08f286a5202f66a0
Michael Bestas <mkbestas@lineageos.org>
2024-12-10 19:29:23 +0200

    rhode: Update to T2SRS33.72-22-4-9
    Change-Id: Ie15f954a91ad8af8685ff5cfd8e32845a9bee70f

--- lineage_rhode.mk
+++ lineage_rhode.mk
@@ -23,6 +23,6 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="rhode_g-user 13 T2SRS33.72-22-4-8 07e10-4fa4c release-keys" \
-    BuildFingerprint=motorola/rhode_g/rhode:13/T2SRS33.72-22-4-8/07e10-4fa4c:user/release-keys \
+    BuildDesc="rhode_g-user 13 T2SRS33.72-22-4-9 e5f56-2298ec release-keys" \
+    BuildFingerprint=motorola/rhode_g/rhode:13/T2SRS33.72-22-4-9/e5f56-2298ec:user/release-keys \
     DeviceProduct=rhode_g
