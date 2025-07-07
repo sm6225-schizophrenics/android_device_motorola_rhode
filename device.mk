5f51d154a20d79f1981bfe7b2be6f1b649114e84
Deivid21 <david.parra.ignacio@gmail.com>
2025-07-07 01:51:43 -0400

    rhode: overlay: Add wifi overlay and MMS UA profile
    Change-Id: Ida9a7044d13feb96b310c3d6f88fb801ba5df98d

--- device.mk
+++ device.mk
@@ -38,7 +38,8 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    WifiResDevice
 
 # Permissions
 PRODUCT_COPY_FILES += \
