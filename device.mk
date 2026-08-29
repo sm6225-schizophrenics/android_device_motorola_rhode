ef28255a91b0e9ba8c2dccbd551983f527564d8b
tigelaa <tigelaa@proton.me>
2026-08-29 11:39:54 -0300

    rhode: drop qti nfc component
    rhode uses the samsung nfc stack, so avoid pulling in the nxp hal implementation.

--- device.mk
+++ device.mk
@@ -49,10 +49,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
     
-# QTI
-TARGET_COMMON_QTI_COMPONENTS += \
-    nfc
-
 # Shipping API level
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := 31
