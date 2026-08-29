e2c23dcf10a0c4d9324d8b127823cd52ef39d793
tigelaa <tigelaa@proton.me>
2026-08-29 11:44:56 -0300

    rhode: define target architectures
    The common tree no longer provides device architecture flags.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,17 @@ include device/motorola/sm6225-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/motorola/rhode
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a73
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_VARIANT := cortex-a73
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := rhode
 
